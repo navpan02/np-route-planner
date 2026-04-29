3a6c6dbbadef579e268ab708029b5e0c4e7d38f6
NP <navpan@gmail.com>
2026-04-28 21:10:03 -0500

    fix: add geoman to optimizeDeps for CI compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/np-route-planner/',
+  optimizeDeps: {
+    include: ['@geoman-io/leaflet-geoman-free'],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/@geoman-io\/leaflet-geoman-free/, /node_modules/],
+    },
+  },
 })
